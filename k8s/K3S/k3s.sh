3f18532f3282300084ce3253373c48e282e08522
Anton Evseev <slayoffer@gmail.com>
2023-12-25 15:12:09 +1000

    changes

--- k8s/K3S/k3s.sh
+++ k8s/K3S/k3s.sh
@@ -27,11 +27,11 @@ KVVERSION="v0.6.3"
 k3sVersion="v1.26.10+k3s2"
 
 # Set the IP addresses of the master and work nodes
-master1=192.168.1.58
-master2=192.168.1.51
-master3=192.168.1.83
-worker1=192.168.1.122
-worker2=192.168.1.138
+master1=192.168.3.21
+master2=192.168.3.22
+master3=192.168.3.23
+worker1=192.168.3.24
+worker2=192.168.3.25
 
 # User of remote machines
 user=ubuntu
@@ -40,7 +40,7 @@ user=ubuntu
 interface=eth0
 
 # Set the virtual IP address (VIP)
-vip=192.168.1.50
+vip=192.168.3.50
 
 # Array of master nodes
 masters=($master2 $master3)
@@ -55,10 +55,10 @@ all=($master1 $master2 $master3 $worker1 $worker2)
 allnomaster1=($master2 $master3 $worker1 $worker2)
 
 #Loadbalancer IP range
-lbrange=192.168.1.151-192.168.1.200
+lbrange=192.168.3.60-192.168.3.80
 
 #ssh certificate name variable
-certName=id_rsa
+certName=ubuntu_id_rsa
 
 #############################################
 #            DO NOT EDIT BELOW              #
@@ -117,7 +117,7 @@ k3sup install \
   --tls-san $vip \
   --cluster \
   --k3s-version $k3sVersion \
-  --k3s-extra-args "--disable traefik --disable servicelb --flannel-iface=$interface --node-ip=$master1" \
+  --k3s-extra-args "--disable traefik --disable servicelb --flannel-iface=$interface --node-ip=$master1 --node-taint node-role.kubernetes.io/master=true:NoSchedule" \
   --merge \
   --sudo \
   --local-path $HOME/.kube/config \
@@ -152,7 +152,7 @@ for newnode in "${masters[@]}"; do
     --server \
     --server-ip $master1 \
     --ssh-key $HOME/.ssh/$certName \
-    --k3s-extra-args "--disable traefik --disable servicelb --flannel-iface=$interface --node-ip=$newnode" \
+    --k3s-extra-args "--disable traefik --disable servicelb --flannel-iface=$interface --node-ip=$newnode --node-taint node-role.kubernetes.io/master=true:NoSchedule" \
     --server-user $user
   echo -e " \033[32;5mMaster node joined successfully!\033[0m"
 done
